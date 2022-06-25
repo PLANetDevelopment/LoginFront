d36c977270cadd69e332f07c50e513b312b03dfc
listenyoon <listenyoon@gmail.com>
2022-06-26 00:04:39 +0900

    mod axios url

--- src/component/OAuth.js
+++ src/component/OAuth.js
@@ -6,15 +6,14 @@ function Oauth() {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
   useEffect(() => {
-    window.localStorage.setItem("token", code);
     (async () => {
       try {
         console.log("try");
         const res = await axios.get(
-          `https://플랜잇.웹.한국:8080/oauth/token?code=${code}`
+          `http://localhost:8080/oauth/token?code=${code}`
         );
         //const token = res.headers.authorization;
-        console.log("complete");
+        console.log(res);
         navigate("/");
       } catch (e) {
         console.log(e);
